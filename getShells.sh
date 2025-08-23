dfedd895e1566c7a39f55b5988e370e5c053dab1
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-08-23 09:36:21 +0200

    Added script
    Added a script to get currently available shells.
    
    See description how to change current login shell.

---
+++ getShells.sh
@@ -0,0 +1,12 @@
+# Author: IT-Administrators
+# License: UNLICENSE
+# OS: Debian
+# /bin/bash <scriptname>
+
+# ----DESCRIPTION
+# Get available login shells. To change the current login shell use:
+# chsh -s <shell path>
+# ----END
+
+#!/bin/bash
+cat /etc/shells
